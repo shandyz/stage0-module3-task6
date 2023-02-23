6d853868b5bbf97b5629e214084d9381ea7319da
shandyz <axshandyz@gmail.com>
2023-02-23 17:06:37 +0100

    test number 7

--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task6;
 
 public class IntegerTypeCasting {
     public static void main(String[] args) {
+        int first = 1000;
+        int second = first;
+        System.out.println(first);
+        System.out.println(second);
     }
 }
